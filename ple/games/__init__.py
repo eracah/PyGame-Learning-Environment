a20b3ed4d00b52a9e518f106e1f61634cefdb8e8
eracah <ejracah@gmail.com>
2018-10-18 03:51:11 -0400

    merged in Rachit Dubey's Human RL Prior games

--- ple/games/__init__.py
+++ ple/games/__init__.py
@@ -11,3 +11,9 @@ from ple.games.puckworld import PuckWorld
 from ple.games.raycastmaze import RaycastMaze
 from ple.games.snake import Snake
 from ple.games.waterworld import WaterWorld
+from ple.games.originalgame import originalGame
+from ple.games.nosemantics import nosemantics
+from ple.games.nosimilarity import nosimilarity
+from ple.games.noobject import noobject
+from ple.games.noaffordance import noaffordance
+from ple.games.continualgame import continualgame
